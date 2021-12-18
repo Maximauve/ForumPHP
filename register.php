78eda56b449ecc914454c79aebf3427b8e421d52
MFERREIRASILVA <matteo.ferreirasilva@ynov.com>
2021-12-18 17:10:44 +0100

    Photo de profil

--- register.php
+++ register.php
@@ -18,7 +18,7 @@ try {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" type="text/css" href="./style/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/style/style.css">
 </head>
 
 <?php 
@@ -46,33 +46,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $file_tmp = $_FILES['picture']['tmp_name'];
             $file_type = $_FILES['picture']['type'];
             $extensions = array("jpeg","jpg","png","gif");
-            $file_ext = strtolower(end(explode('.',$file_name)));
+            $tmp = explode('.', $file_name);
+            $file_ext = end($tmp);
             if(!in_array($file_ext,$extensions) || $file_size > 4000000) {
                 $errorMessage = "Votre photo n'est pas conforme !";
                 return;
             }
-            $upload = move_uploaded_file($file_tmp,"./pictures/".$file_name);
+            $upload = move_uploaded_file($file_tmp,"./assets/profile_pictures/".$username . "." . $file_ext);
             if (!$upload) {
                 $errorMessage = "Erreur dans le téléchargement de votre photo";
                 return;
             }
-            $picture = "./pictures/" . $file_name;
+            $picture = "./assets/profile_pictures/" . $username. "." . $file_ext;
         }
         $password = $_POST['password'];
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $queryString  = "INSERT INTO user (username, password, mail, picture ) VALUES (:username, :password, :mail, :picture)";
-        $datas = [
-            'username'=>$username,
-            'password'=>$password,
-            'mail'=>$mail,
-            'picture'=>$picture
-        ];
-        $query = $pdo->prepare($queryString);
-        $query->execute($datas);
-        $_SESSION['username'] = htmlspecialchars($_POST['username']);
-        $_SESSION['connected'] = true;
-        header('Location: /');
-        die();
+        $confirm = $_POST['confirmpassword'];
+        if ($password !== $confirm) {
+            $errorMessage = "Les mots de passe ne correspondent pas.";
+        } else {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $queryString  = "INSERT INTO user (username, password, mail, picture ) VALUES (:username, :password, :mail, :picture)";
+            $datas = [
+                'username'=>$username,
+                'password'=>$password,
+                'mail'=>$mail,
+                'picture'=>$picture
+            ];
+            $query = $pdo->prepare($queryString);
+            $query->execute($datas);
+            $_SESSION['username'] = htmlspecialchars($_POST['username']);
+            $_SESSION['connected'] = true;
+            header('Location: /');
+            die();
+        }
     }
 }
 
@@ -97,21 +103,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <br>
         <label for="password">
             Mot de passe* :
-            <input id='password' type="password" name="password" placeholder="password" onkeyup='check()' required>
+            <input id='password' type="text" name="password" placeholder="password" onkeyup='check()' required>
         </label>
         <br>
         <label for="password">
             Confirmation de mot de passe* :
-            <input id='confirmpassword' type="password" name="password" placeholder="confirmpassword" onkeyup='check()' required>
+            <input id='confirmpassword' type="text" name="confirmpassword" placeholder="confirm password" onkeyup='check()' required>
         </label>
         <br>
-        <button type="submit" name="login" id="confirmbtn" disabled="true">❌</button>
+        <button type="submit" name="login" id="confirmbtn">Connexion</button>
         <br>
 
         <p id="message"></p>
         <p> <?= $errorMessage ?> </p>
     </form>
     <a href="./login.php"><button name="login">   Tu as deja un compte ? connecte toi  </button></a>
-    <script type="text/javascript" src="./js/script.js"></script>
+    <!-- <script type="text/javascript" src="./assets/js/script.js"></script> -->
 </body>
 </html>
