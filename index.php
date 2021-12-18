291ef0a7cbadcc47ff319d6d3e1f7f9a1c9261b0
Maxime Mourgues <maxime.mourgues@ynov.com>
2021-12-18 15:40:24 +0100

    made a 'navbar.php' file

--- index.php
+++ index.php
@@ -4,58 +4,29 @@ $dsn="mysql:host=localhost:3306;dbname=forum";
 $username='root';
 $password='';
 try {
-    $pdo = new PDO($dsn, $username, $password);
+	$pdo = new PDO($dsn, $username, $password);
 } catch (PDOException $exception) {
-    die();
+	die();
 }
 
 ?>
 
 <html lang="fr">
 <head>
-    <link rel="stylesheet" type="text/css" href="./style/style.css">
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Forum</title>
+	<link rel="stylesheet" type="text/css" href="./assets/style/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title>Forum</title>
 </head>
 
 <body>
 
+<?php require('./templates/navbar.php'); ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/">Acceuil</a>
-        </li>
-        <?php
-        $username = $_SESSION["username"];
-        $queryCheck = "SELECT admin FROM user WHERE username = :username";
-        $datas = [
-            'username'=>$username,
-        ];
-        $query = $pdo->prepare($queryCheck);
-        $query->execute($datas);
-        $check = $query->fetch();
-        if ($check[0]) { ?>
-            <li class="nav-item">
-              <a class="nav-link" href="./admin.php">Admin</a>
-            </li>
-        <?php } ?>
-        <li class="nav-item">
-          <a class="nav-link" href="./logout.php">Se déconnecter</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<script type="text/javascript" src="./js/script.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script type="text/javascript" src="./assets/js/script.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ templates/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" aria-current="page" href="/">Acceuil</a>
+				</li>
+				<?php
+				$username = $_SESSION["username"];
+				$queryCheck = "SELECT admin FROM user WHERE username = :username";
+				$datas = [
+					'username'=>$username,
+				];
+				$query = $pdo->prepare($queryCheck);
+				$query->execute($datas);
+				$check = $query->fetch();
+				if ($check[0]) { ?>
+					<li class="nav-item">
+						<a class="nav-link" href="./admin.php">Admin</a>
+					</li>
+				<?php } ?>
+				<li class="nav-item">
+					<a class="nav-link" href="./logout.php">Se déconnecter</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
