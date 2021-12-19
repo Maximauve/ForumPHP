cf34a8bf6c4dcdf53eab34acc1a5138ff2bf1bcb
Maxime Mourgues <maxime.mourgues@ynov.com>
2021-12-19 20:22:43 +0100

    CSS

--- profile.php
+++ profile.php
@@ -18,15 +18,12 @@ try {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Forum</title>
 </head>
 
 <body>
 
-<?php require('./templates/navbar.php'); ?>
-
-<?php
+<?php require('./templates/navbar.php');
 
 $queryUser = "SELECT * FROM user WHERE username = :username";
 $datas = [
@@ -54,28 +51,26 @@ $resPosts = $queryP->fetchAll(PDO::FETCH_ASSOC);
 <h1> RECUPERATIONS DES POSTS </h1>
 
 <?php foreach($resPosts as $post) {?>
-    <div class="card mb-3">
-    <div class="row g-0">
-        <?php if ($post["picture"]) {?>
-            <div class="col-md-6">
-        <?php } ?>
-            <div class="card-body">
-            <h5 class="card-title"><?=$post["title"]?></h5>
-                <p class="card-text"><?=$post["content"]?></p>
-                <p class="card-text"><small class="text-muted"><?=$post["publicationDate"]?></small></p>
-            </div>
-        <?php if ($post["picture"]) {?>
-            </div>
-            <div class="col-md-6">
-                <img src="<?=$post["picture"]?>" class="card-img-rigth"">
-            </div>
-        <?php } ?>
-    </div>
-</div>
-    <?php } ?>
+	<div class="posts">
+  	<div class="card-post">
+  	  <?php if ($post["picture"]) {?>
+  	    <div>
+  	  <?php } ?>
+  	  <div>
+  	  	<p class="post-title"><?=$post["title"]?></p>
+  	  	<p class="post-content"><?=$post["content"]?></p>
+  	  	<p class="post-date"><?=$post["publicationDate"]?></p>
+  	  </div>
+  	  <?php if ($post["picture"]) {?>
+  	  	</div>
+  	  	<div>
+  	  		<img src="<?=$post["picture"]?>">
+  	  	</div>
+  		<?php } ?>
+		</div>
+	</div>
+<?php } ?>
 
 <script type="text/javascript" src="./assets/js/script.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
--- templates/navbar.php
+++ templates/navbar.php
@@ -1,34 +1,30 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<div class="container-fluid">
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link" aria-current="page" href="/">Acceuil</a>
-				</li>
-				<?php
-				$username = $_SESSION["username"];
-				$queryCheck = "SELECT admin FROM user WHERE username = :username";
-				$datas = [
-					'username'=>$username,
-				];
-				$query = $pdo->prepare($queryCheck);
-				$query->execute($datas);
-				$check = $query->fetch();
-				if ($check[0]) { ?>
-					<li class="nav-item">
-						<a class="nav-link" href="./admin.php">Admin</a>
-					</li>
-				<?php } ?>
-				<li class="nav-item">
-					<a class="nav-link" href="./profile.php">Profile</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="./new_post.php">Nouveau Post</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="./logout.php">Se déconnecter</a>
-				</li>
-			</ul>
-		</div>
-	</div>
+<nav>
+	<ul class="navbar">
+		<li class="first navbar-li">
+			<a class="nav-link" href="./profile.php">Profile</a>
+		</li>
+		<li class="navbar-li">
+			<a class="nav-link" href="/">Acceuil</a>
+		</li>
+		<?php
+		$username = $_SESSION["username"];
+		$queryCheck = "SELECT admin FROM user WHERE username = :username";
+		$datas = [
+			'username'=>$username,
+		];
+		$query = $pdo->prepare($queryCheck);
+		$query->execute($datas);
+		$check = $query->fetch();
+		if ($check[0]) { ?>
+			<li class="navbar-li">
+				<a class="nav-link" href="./admin.php">Admin</a>
+			</li>
+		<?php } ?>
+		<li class="navbar-li">
+			<a class="nav-link" href="./new_post.php">Nouveau Post</a>
+		</li>
+		<li class="navbar-li">
+			<a class="nav-link" href="./logout.php">Se déconnecter</a>
+		</li>
+	</ul>
 </nav>
