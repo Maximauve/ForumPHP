5fd3595d5bc88a15892fe68483a0c2703c09e96a
MFERREIRASILVA <matteo.ferreirasilva@ynov.com>
2021-12-20 15:44:56 +0100

    Ajout de la fonctionalité edit

--- edit.php
+++ edit.php
@@ -8,25 +8,47 @@ try {
 } catch (PDOException $exception) {
     die();
 }
-
+$errorMessage = "";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$queryDelete = "SELECT * FROM article WHERE id = :id";
-    $datas = [
-        'id'=>$_POST["id"],
-    ];
-    $query = $pdo->prepare($queryDelete);
-    $query->execute($datas);
-    $post = $query->fetch(mode:PDO::FETCH_ASSOC);
-    
+    if ($_POST["id"]) {
+        $queryDelete = "SELECT * FROM article WHERE id = :id";
+        $datas = [
+            'id'=>$_POST["id"],
+        ];
+        $query = $pdo->prepare($queryDelete);
+        $query->execute($datas);
+        $post = $query->fetch(mode:PDO::FETCH_ASSOC);
+    } else {
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+        $articleId = $_POST['articleId'];
+        $queryUpdate = "UPDATE article SET title = :title, content = :content WHERE id = :articleId";
+        $datas = [
+            'title'=>$title,
+            'content'=>$content,
+            'articleId'=>$articleId
+        ];
+        $query = $pdo->prepare($queryUpdate);
+        $query->execute($datas);
+        header('Location: /');
+        die();
+    }
 } else {
 	header('Location: /');
 }
 
 ?>
+<html lang="fr">
+<?php require("./templates/head.php"); ?>
+
+<body>
+
+
+<?php require('./templates/navbar.php'); ?>
 
 <h1> INFORMATIONS <h2>
 
-<form action="./new_post.php" method="post" enctype="multipart/form-data">
+<form action="./edit.php" method="post" enctype="multipart/form-data">
     <label for="title">
         Titre :
         <input type="text" name="title" value="<?= $post["title"] ?>" required autofocus>
@@ -37,7 +59,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <input type="text" name="content" value="<?= $post["title"] ?>" required>
     </label>
     <br>
+    <input type="text" name="articleId" value="<?=$post["id"]?>" style="display: none;"/>
     <button type="submit" name="login" id="confirmbtn">Edit</button>
     <br>
     <p> <?= $errorMessage ?> </p>
 </form>
+</body>
+</html>
